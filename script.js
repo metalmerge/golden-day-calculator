3ed393b90859d04cd190048fcf0872daea699079
Metalmerge <ermakovd06@gmail.com>
2023-07-11 19:24:21 -0400

    dynamic list addition

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-function calculateGoldenDay() {
-    var date = document.getElementById("date").value;
-    var result = document.getElementById("result");
-    
-    // Perform calculation
-    // ...
-    
-    result.innerHTML = "The golden score for " + date + " is X.";
-}
-
-// Chart.js example
-var ctx = document.getElementById('chart').getContext('2d');
-var chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Data Point 1', 'Data Point 2', 'Data Point 3'],
-        datasets: [{
-            label: 'Golden Scores',
-            data: [5, 8, 3],
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: true,
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    const array = [
+      { name: 'Religion', votes: 53, coalition: 'Blue' },
+      { name: 'Motivation', votes: 37, coalition: 'Blue' },
+      { name: 'Maintenance', votes: 37, coalition: 'Blue' },
+      { name: 'Mental', votes: 42, coalition: 'Red' },
+      { name: 'Education', votes: 32, coalition: 'Blue' },
+      { name: 'Sports', votes: 47, coalition: 'Blue' },
+      { name: 'Exercise', votes: 41, coalition: 'Blue' },
+      { name: 'Watch', votes: 20, coalition: 'Red' },
+      { name: 'Work', votes: 50, coalition: 'Green' },
+      { name: 'Pomodomo', votes: 30, coalition: 'Blue' },
+      { name: 'Invest', votes: 0, coalition: 'Green' },
+      { name: 'Streak', votes: 0, coalition: 'Green' },
+      { name: 'Sleep', votes: 50, coalition: 'Red' },
+      { name: 'Music', votes: 20, coalition: 'Red' },
+      { name: 'Social', votes: 17, coalition: 'Red' },
+      { name: 'Fun', votes: 6, coalition: 'Red' },
+      { name: 'Programming', votes: 48, coalition: 'Blue' },
+      { name: 'Knowledge', votes: 20, coalition: 'Blue' }
+    ];
+  
+    const againstItems = document.getElementById('againstItems');
+    const forItems = document.getElementById('forItems');
+  
+    // Populate initial against list
+    array.forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = `${item.name}: ${item.votes}`;
+      li.setAttribute('data-coalition', item.coalition);
+      li.addEventListener('click', () => switchList(li));
+      againstItems.appendChild(li);
+    });
+  
+    function switchList(li) {
+      const parentList = li.parentNode;
+      const targetList = parentList.id === 'againstItems' ? forItems : againstItems;
+      parentList.removeChild(li);
+      targetList.appendChild(li);
     }
-});
+  });
+  
